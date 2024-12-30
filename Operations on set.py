76f049dc70785cdc3d835df5cb1dd283fe7cfcd4
toxic123MMM <aashish02@gmail.com>
2024-12-30 17:26:21 +0400

    Add files via upload

---
+++ Operations on set.py
@@ -0,0 +1,14 @@
+my_set={1,2,3,4,5}
+print(my_set)
+
+my_set1={1,2,3,4,3,1}
+print(my_set1)
+
+my_set2={1.20,"Codingal",(1,2,3,4)}
+print(my_set2)
+
+my_set3=set([1,2,3,4,5,6,7,8,9,])
+print("OrignalSet",my_set3)  
+print("After copying the first element")
+my_set3.pop()
+print(my_set3)
